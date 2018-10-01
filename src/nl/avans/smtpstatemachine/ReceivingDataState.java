497b9cfa3d08af6ac8b94138bcfd76ecd4d4b5c3
Dylan ten Böhmer <djhbohme@avans.nl>
2018-10-01 11:34:04 +0200

    bla

--- src/nl/avans/smtpstatemachine/ReceivingDataState.java
+++ src/nl/avans/smtpstatemachine/ReceivingDataState.java
@@ -3,25 +3,33 @@ import nl.avans.SmtpContext;
 public class ReceivingDataState implements SmtpStateInf {
     SmtpContext context;
     boolean crReceived; // What is this?
-    boolean dotReceived;
+    boolean dotReceived = false;
     public ReceivingDataState(SmtpContext context) {
         this.context = context;
     }
 
     @Override
     public void Handle(String data) {
+        /*if (dotReceived = true)
+        {
+            context.SetNewState(new WaitForMailFromState(context));
+        }*/
         context.AddTextToBody(data);
-        if (data.toUpperCase().endsWith(".")) {
+        if (data.toUpperCase().endsWith("."))  {
             dotReceived = true;
             context.SendData("250 Ok: queued as 12345");
             context.SetNewState(new WaitForMailFromState(context));
             return;
         }
+        /*else
+        {
+            dotReceived = false;
+            context.SetNewState(this);
+        }*/
         if (data.toUpperCase().startsWith("QUIT")) {
             context.SendData("221 Bye");
             context.DisconnectSocket();
-            return;
         }
-        return;
+        context.SendData("...");
     }
 }
